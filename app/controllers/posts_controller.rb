a57145f96ba7e9da02c1dacb2292aa2a85765a02
srinivas-sateesh <srinivas.sateesh@gmail.com>
2016-04-23 10:49:31 +0000

    Added code for controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,54 @@
 class PostsController < ApplicationController
+  
+  before_action :find_posts, only: [:show,:edit,:update,:destroy]
+  
+  
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   def show
+    
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def edit
+    
   end
 
   def update
+    if @post.update(update_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
-  def delete
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :link, :description)
+  end
+  
+  def find_post
+    @post = Post.find(params[:id])
+  end
+  
 end
