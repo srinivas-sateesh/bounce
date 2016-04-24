81ee3b8360b08e4320b3258463b45cf7214c2176
srinivas-sateesh <srinivas.sateesh@gmail.com>
2016-04-24 06:26:19 +0000

    Added Assocsiations between Users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424060736) do
+ActiveRecord::Schema.define(version: 20160424062231) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160424060736) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
